10843b3b4a02c6b07b2247565466eb466ca5838f
tdas <tdas@zebestof.com>
2025-09-04 14:35:32 +0200

    wip

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -584,18 +584,6 @@ export default function DashboardPage() {
                     <CardTitle className="text-sm font-medium">
                       Prix moyen
                     </CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-primary"
-                    >
-                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
                   </CardHeader>
                   <CardContent className="pt-4">
                     <div className="text-2xl font-bold">
@@ -611,18 +599,6 @@ export default function DashboardPage() {
                     <CardTitle className="text-sm font-medium">
                       Prix médian
                     </CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-primary"
-                    >
-                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
                   </CardHeader>
                   <CardContent className="pt-4">
                     <div className="text-2xl font-bold">
